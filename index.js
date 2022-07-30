5f74dcd5ef7876d10a2b790e385e3a186c4b960c
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-31 00:28:16 +0600

    create post api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+require('dotenv').config();
 const cors = require('cors');
 
 app.use(cors());
@@ -10,6 +10,13 @@ app.use(express());
 app.get('/', (req, res) => {
     res.send('Testing JWT Server')
 })
+
+// login route
+app.post('/login', (req, res) =>{
+    const user = req.body;
+    console.log(user);
+    res.send({success: true});
+});
 
 app.listen(port, (req, res) =>{
     console.log(" Listening to port ", port);
