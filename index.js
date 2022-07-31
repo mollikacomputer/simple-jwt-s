55de27ec8db9eea3ede901de3c3519d16d0d929a
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-31 23:34:06 +0600

    jwt access token created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -15,8 +16,16 @@ app.get('/', (req, res) => {
 // login route
 app.post('/login', (req, res) =>{
     const user = req.body;
-    res.send({success: true})
     console.log(user);
+    if(user.email ==='user@gmail.com' && user.password === '123456'){
+        const accessToken = jwt.sign({email: user.email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+        res.send({
+            success: true,
+            accessToken: accessToken
+        })
+    }else{
+        res.send({success: false})
+    }
 })
 
 
