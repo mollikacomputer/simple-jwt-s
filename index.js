52596ec86856852ad3ce256b2949d585c3ce07c3
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-02 03:51:31 +0600

    verifyJWT function created for middleware to use one more places

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+// function for middleware
+
+const verifyJWT = (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+    console.log('inside verify token', authHeader);
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized'});
+    }
+    const token = authHeader.split(' ')[1];
+}
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -18,7 +29,12 @@ app.post('/login', (req, res) =>{
     const user = req.body;
     console.log(user);
     if(user.email ==='user@gmail.com' && user.password === '123456'){
-        const accessToken = jwt.sign({email: user.email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+        // for JWT access token
+        const accessToken = jwt.sign(
+            {email: user.email}, 
+            process.env.ACCESS_TOKEN_SECRET, 
+            {expiresIn:'1h'}
+            )
         res.send({
             success: true,
             accessToken: accessToken
@@ -26,9 +42,19 @@ app.post('/login', (req, res) =>{
     }else{
         res.send({success: false})
     }
-})
+});
 
+app.get('/orders', verifyJWT, (req, res) => {
+    // for test get token local storage to server
+    // console.log(req.headers.authorization);
+    res.send([{id:1, item: 'sunglass'}, {id:2, item: 'moonglass'} ])
+})
 
 app.listen(port, (req, res) =>{
     console.log(" Listening to port ", port);
 });
+
+// create jwt web token 
+//1st step require('crypto').randomBytes(64)
+//2nd step require('crypto').randomBytes(64).toString()
+// 3rd step require('crypto').randomBytes(64).toString('hex')
