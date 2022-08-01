366dfe71af0ae18171e8fd2f9ea36dbfe5a78036
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-02 05:31:45 +0600

    some modify for jwt token

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const verifyJWT = (req, res, next) =>{
         return res.status(401).send({message:'unauthorized'});
     }
     const token = authHeader.split(' ')[1];
+    jwt.verify( token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+        return res.status(403).send({message: 'forbidden'})
+    }
+    req.decoded= decoded;
+    next();
+    })
 }
 
 // middleware
